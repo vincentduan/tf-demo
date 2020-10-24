e017c50726c1ea71e69a4b4589f64cb6d4f1e7fb
duandingyang <1@qq.com>
2020-10-24 19:46:16 +0800

    update

--- test5.py
+++ test5.py
@@ -2,7 +2,7 @@ from matplotlib.colors import ListedColormap
 import numpy as np
 
 def plot_desision_regions(X, y, classifier, resolution=0.02):
-    marker = ('s', 'x', 'o', 'v');
+    marker = ('s', 'x', 'o', 'v')
     colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
     cmap = ListedColormap(colors[:len(np.unique(y))])
 
